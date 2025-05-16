9d6b1b1bece6845b7bc77aeea79aefda35a93d54
siwon <muncool39@gmail.com>
2025-05-17 03:56:59 +0900

    [TEST] Testcontainers 사용해서 LocalStack 통합 테스트

---
+++ src/main/java/com/example/test/config/S3Config.java
@@ -0,0 +1,33 @@
+package com.example.test.config;
+
+
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+
+
+@Configuration
+public class S3Config {
+    public static final String BUCKET_NAME = "test-bucket";
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public LocalStackContainer localStackContainer(){
+        return new LocalStackContainer()
+                .withServices(S3);
+    }
+
+    @Bean
+    public AmazonS3 amazonS3(LocalStackContainer localStackContainer){
+        AmazonS3 amazonS3 = AmazonS3ClientBuilder
+                .standard()
+                .withEndpointConfiguration(localStackContainer.getEndpointConfiguration(S3))
+                .withCredentials(localStackContainer.getDefaultCredentialsProvider())
+                .build();
+        amazonS3.createBucket(BUCKET_NAME);
+        return amazonS3;
+    }
+}
